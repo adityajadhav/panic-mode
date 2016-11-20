b007f534dfb75e24f3ba6c11feb05fa1c69c6858
Nikhita Kataria <nikhitakataria@gmail.com>
2016-11-20 09:37:57 -0600

    Comitting Server Changes

---
+++ rest_service/rest.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+import web
+import clarifai
+import sys
+from io import StringIO
+from clarifai.rest import Image as ClImage
+from clarifai.rest import ClarifaiApp
+import requests
+import os
+import json
+
+url='https://fcm.google.apis.com/fcm/send'
+
+app = ClarifaiApp()
+app.auth.get_token()
+model = app.models.get('general-v1.3')
+
+urls = (
+    '/getTags', 'getClarifaiTags',
+)
+
+app = web.application(urls, globals())
+
+def post():
+        url='https://fcm.googleapis.com/fcm/send'
+        myheaders={'Content-Type':'application/json', 
+                   'Authorization':'key=AIzaSyA5auBbUXjODOeCaM4pe6TyIDCJVwYBLy4'}
+        payload={
+            "to": "/topics/allDevices",
+              "notification": {
+                "body" : "greate match!",
+                "title" : "You vs Me"
+              }
+        }
+        response = requests.post(url,data=json.dumps(payload), headers=myheaders)
+
+class getClarifaiTags:        
+    def GET(self):
+        os.system("wget 10.105.94.11:8080/shot.jpg");
+        image = ClImage(file_obj=open('/home/root/rest_service/shot.jpg', 'rb'))
+        results = model.predict([image])
+        setofprediction = results['outputs'][0]['data']['concepts']
+        for i in setofprediction:
+              if (i['name'] == 'stress' || i['name'] == 'panic'):
+                 post(); 
+        os.system("rm -rf shot.jpg");
+        return "Works"
+
+if __name__ == "__main__":
+    app.run()
+
+
+#// LEverage the tries and tested methods that already exist
---
+++ rest_service/rest_demo.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import clarifai
+import sys
+from io import StringIO
+from clarifai.rest import Image as ClImage
+from clarifai.rest import ClarifaiApp
+import requests
+import os
+import json
+
+app = ClarifaiApp()
+app.auth.get_token()
+model = app.models.get('general-v1.3')
+
+def post():
+        url='https://fcm.googleapis.com/fcm/send'
+        myheaders={'Content-Type':'application/json', 
+                   'Authorization':'key=AIzaSyA5auBbUXjODOeCaM4pe6TyIDCJVwYBLy4'}
+        payload={
+            "to": "/topics/allDevices",
+              "notification": {
+                "body" : "greate match!",
+                "title" : "You vs Me"
+              }
+        }
+        response = requests.post(url,data=json.dumps(payload), headers=myheaders)
+
+def detect():
+        os.system("wget 10.105.94.11:8080/shot.jpg");
+        image = ClImage(file_obj=open('/home/root/rest_service/shot.jpg', 'rb'))
+        results = model.predict([image])
+        os.system("rm -rf shot.jpg");
+        setofprediction = results['outputs'][0]['data']['concepts']
+        for i in setofprediction:
+              print i['name']
+              if (i['name']=="industry" or i['name']=="can" or i['name'] is "injury" or i['name'] is "accident"):
+                  post(); 
+                  sys.exit(0)
+
+if __name__ == "__main__":
+    while 1:
+        detect()
+
